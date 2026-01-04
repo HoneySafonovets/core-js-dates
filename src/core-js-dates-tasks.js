92e162fa0103ef097895e6badcd4502cfd24865b
Danil <kfs.boi@yandex.ru>
2026-01-04 22:38:02 +0300

    feat: add function formatDate

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -180,8 +180,25 @@ function isDateInPeriod(date, period) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  const newDate = new Date(date);
+  let diffTime = 0;
+  let timePart = '';
+
+  if (newDate.getUTCHours() === 12) {
+    diffTime = newDate.getUTCHours();
+    timePart = 'PM';
+  } else if (newDate.getUTCHours() > 12) {
+    diffTime = newDate.getUTCHours() - 12;
+    timePart = 'PM';
+  } else {
+    diffTime = newDate.getUTCHours();
+    timePart = 'AM';
+  }
+
+  const timeString = `${diffTime.toString()}:${newDate.getUTCMinutes().toString().padStart(2, 0)}:${newDate.getUTCSeconds().toString().padStart(2, 0)} ${timePart}`;
+
+  return `${newDate.getUTCMonth() + 1}/${newDate.getUTCDate()}/${newDate.getUTCFullYear()}, ${timeString}`;
 }
 
 /**
